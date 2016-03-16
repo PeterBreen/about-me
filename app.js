c295b089faaa36f7bd960887164f037d7b9759c3
Peter Breen <pbreen@gmail.com>
2016-03-16 09:56:54 -0700

    first question functional/template for rest

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
- var favoriteNumber = prompt('What is your favorite number?');
-alert('your favorite number is ' + favoriteNumber);
-console.log('Favorite number: ' + favoriteNumber);
+var userName = prompt('What is your name?');
+alert('Nice to meet you, ' + userName + '.');
+console.log('userName is ' + userName);
 
-var favoriteColor = prompt('What is your favorite color?');
-alert('your favorite color is ' + favoriteColor);
-console.log('Favorite color: ' + favoriteNumber);
+var beenJoshuaTree;
+var beenJoshuaTree = prompt('Have I ever been to Joshua Tree?');
+if ((beenJoshuaTree.toLowerCase() === 'yes') || (beenJoshuaTree.toLowerCase() === 'y')) {
+  alert(userName + ', that is right! I have been to Joshua tree.');
+  console.log(userName + ', that is right! I have been to Joshua tree.');
+} else if ((beenJoshuaTree.toLowerCase() === 'no') || (beenJoshuaTree.toLowerCase() === 'n')) {
+  alert('Sorry, ' + userName + ' that is not correct. I have been to Joshua Tree.');
+  console.log('Sorry, ' + userName + ' that is not correct. I have been to Joshua Tree.');
+} else {
+  alert('Please answer with either yes/no or y/n.');
+  console.log('Please answer with either yes/no or y/n.');
+}
 
-var airspeedUnladenSwallow = prompt('What is the airspeed of an unladen swallow? European and in kph, to be precise.');
-alert('You said the airspeed is ' + airspeedUnladenSwallow + ' kph');
-console.log('Airspeed in kph: ' + airspeedUnladenSwallow);
-
-var camelotOpinion = confirm('Is Camelot a silly place?');
-alert('Camelot is pretty silly? ' + camelotOpinion + '!');
-console.log('Is Camelot silly? The answer is ' + camelotOpinion);
-
-var coconuts = confirm('Are coconuts an acceptable horse substitute?');
-alert('Coconuts are an acceptable substitute: ' + coconuts);
-console.log('Can you believe they answered that coconuts are acceptable like this? ' + coconuts);
-
-var tiredJoke = confirm('Sick of seeing Monty Python references?');
-alert('I\'m tired of them. You, though? You said: ' + tiredJoke);
-console.log('Tired of this joke status: ' + tiredJoke);
+/* reference from code demo yesterday
+var agreeWithConditions = prompt('Do you agree with me? yes or no');
+var yesString = 'yes';
+if(agreeWithConditions.toUpperCase() === yesString.toUpperCase()){
+  console.log('hurray! you agreed!');
+} else {
+  console.log('boooo, you did not agree');
+}
+*/
