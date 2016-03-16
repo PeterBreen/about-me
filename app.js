e9657e0196bd2ab89cfd349859fa323a07721f32
Peter Breen <pbreen@gmail.com>
2016-03-16 08:28:38 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+ var favoriteNumber = prompt('What is your favorite number?');
+alert('your favorite number is ' + favoriteNumber);
+console.log('Favorite number: ' + favoriteNumber);
+
+var favoriteColor = prompt('What is your favorite color?');
+alert('your favorite color is ' + favoriteColor);
+console.log('Favorite color: ' + favoriteNumber);
+
+var airspeedUnladenSwallow = prompt('What is the airspeed of an unladen swallow? European and in kph, to be precise.');
+alert('You said the airspeed is ' + airspeedUnladenSwallow + ' kph');
+console.log('Airspeed in kph: ' + airspeedUnladenSwallow);
+
+var camelotOpinion = confirm('Is Camelot a silly place?');
+alert('Camelot is pretty silly? ' + camelotOpinion + '!');
+console.log('Is Camelot silly? The answer is ' + camelotOpinion);
+
+var coconuts = confirm('Are coconuts an acceptable horse substitute?');
+alert('Coconuts are an acceptable substitute: ' + coconuts);
+console.log('Can you believe they answered that coconuts are acceptable like this? ' + coconuts);
+
+var tiredJoke = confirm('Sick of seeing Monty Python references?');
+alert('I\'m tired of them. You, though? You said: ' + tiredJoke);
+console.log('Tired of this joke status: ' + tiredJoke);
