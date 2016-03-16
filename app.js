bf3c55066a4d93428973fd18515bc484a3504269
Peter Breen <pbreen@gmail.com>
2016-03-16 13:31:10 -0700

    remove extraneous () from app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ console.log('userName is ' + userName);
 
 //first question
 var beenJoshuaTree = prompt('Have I ever been to Joshua Tree?');
-if ((beenJoshuaTree.toLowerCase() === 'yes') || (beenJoshuaTree.toLowerCase() === 'y')) {
+if (beenJoshuaTree.toLowerCase() === 'yes' || beenJoshuaTree.toLowerCase() === 'y') {
   alert(userName + ', that is right! I have been to Joshua Tree.');
   console.log('Q1: ' + userName + ', that is right! I have been to Joshua Tree.');
-} else if ((beenJoshuaTree.toLowerCase() === 'no') || (beenJoshuaTree.toLowerCase() === 'n')) {
+} else if (beenJoshuaTree.toLowerCase() === 'no' || beenJoshuaTree.toLowerCase() === 'n') {
   alert('Sorry, ' + userName + ' that is not correct. I have been to Joshua Tree.');
   console.log('Q1: Sorry, ' + userName + ', that is not correct. I have been to Joshua Tree.');
 } else {
@@ -17,10 +17,10 @@ if ((beenJoshuaTree.toLowerCase() === 'yes') || (beenJoshuaTree.toLowerCase() ==
 
 //second question
 var hikedMountRainier = prompt('Have I ever summited Mt. Rainier?');
-if ((hikedMountRainier.toLowerCase() === 'yes') || (hikedMountRainier.toLowerCase() === 'y')) {
+if (hikedMountRainier.toLowerCase() === 'yes' || hikedMountRainier.toLowerCase() === 'y') {
   alert(userName + ', that is not right. I have never summited Rainier.');
   console.log('Q2: ' + userName + ', that is not right. I have never summited Rainier.');
-} else if ((hikedMountRainier.toLowerCase() === 'no') || (hikedMountRainier.toLowerCase() === 'n')) {
+} else if (hikedMountRainier.toLowerCase() === 'no' || hikedMountRainier.toLowerCase() === 'n') {
   alert('Correct, ' + userName + '! I have been to the park but never summited.');
   console.log('Q2: Correct, ' + userName + '! I have been to the park but never summited.');
 } else {
@@ -30,10 +30,10 @@ if ((hikedMountRainier.toLowerCase() === 'yes') || (hikedMountRainier.toLowerCas
 
 //third question
 var runBadCommand = prompt('Am I ever going to run \'rm -rf /\' ?');
-if ((runBadCommand.toLowerCase() === 'yes') || (runBadCommand.toLowerCase() === 'y')) {
+if (runBadCommand.toLowerCase() === 'yes' || runBadCommand.toLowerCase() === 'y') {
   alert(userName + ', are you kidding? I will never do that.');
   console.log('Q3: ' + userName + ', are you kidding? I will never run that command.');
-} else if ((runBadCommand.toLowerCase() === 'no') || (runBadCommand.toLowerCase() === 'n')) {
+} else if (runBadCommand.toLowerCase() === 'no' || runBadCommand.toLowerCase() === 'n') {
   alert('Absolutely, ' + userName + '! that command is crazy. Not even once.');
   console.log('Q3: Absolutely, ' + userName + ', that command is crazy. Not even once.');
 } else {
@@ -43,10 +43,10 @@ if ((runBadCommand.toLowerCase() === 'yes') || (runBadCommand.toLowerCase() === 
 
 //fourth question
 var haveSibling = prompt('Do I have a sibling?');
-if ((haveSibling.toLowerCase() === 'yes') || (haveSibling.toLowerCase() === 'y')) {
+if (haveSibling.toLowerCase() === 'yes' || haveSibling.toLowerCase() === 'y') {
   alert(userName + ', that is right! I have an older brother.');
   console.log('Q4: ' + userName + ', that is right! I have an older brother.');
-} else if ((haveSibling.toLowerCase() === 'no') || (haveSibling.toLowerCase() === 'n')) {
+} else if (haveSibling.toLowerCase() === 'no' || haveSibling.toLowerCase() === 'n') {
   alert('Sorry, ' + userName + ', that is not correct. I have an older brother.');
   console.log('Q4: Sorry, ' + userName + ', that is not correct. I have an older brother.');
 } else {
@@ -56,21 +56,21 @@ if ((haveSibling.toLowerCase() === 'yes') || (haveSibling.toLowerCase() === 'y')
 
 //fifth question - redirect to question six on yes otherwise end
 var seenStarTrek = prompt('How about we talk about you? Have you ever seen a Star Trek movie?');
-if ((seenStarTrek.toLowerCase() === 'yes') || (seenStarTrek.toLowerCase() === 'y')) {
+if (seenStarTrek.toLowerCase() === 'yes' || seenStarTrek.toLowerCase() === 'y') {
   var seenYes = prompt(userName + ', how about that? Do you think II is my favorite?');
   console.log('Q5: ' + userName + ', how about that? Do you think II is my favorite?');
   //sixth question - only asked if fifth was yes
-  if ((seenYes.toLowerCase() === 'yes') || (seenYes.toLowerCase() === 'y')) {
+  if (seenYes.toLowerCase() === 'yes' || seenYes.toLowerCase() === 'y') {
     alert(userName + ', that is right! Wrath of Khan is pretty great.');
     console.log('Q6: ' + userName + ', that is right! Wrath of Khan is pretty great.');
-  } else if ((seenYes.toLowerCase() === 'no') || (seenYes.toLowerCase() === 'n')) {
+  } else if (seenYes.toLowerCase() === 'no' || seenYes.toLowerCase() === 'n') {
     alert('Sorry, ' + userName + ' I think VI is pretty good, II is my favorite.');
     console.log('Q6: Sorry, ' + userName + ', I think VI is pretty good, II is my favorite.');
   } else {
     alert('Please answer with either yes/no or y/n.');
     console.log('Q6: Please answer with either yes/no or y/n.');
   }
-} else if ((seenStarTrek.toLowerCase() === 'no') || (seenStarTrek.toLowerCase() === 'n')) {
+} else if (seenStarTrek.toLowerCase() === 'no' || seenStarTrek.toLowerCase() === 'n') {
   alert('No problem, ' + userName + ', thank you for answering my quiz!');
   console.log('Q5: No problem, ' + userName + ', thank you for answering my quiz!');
 } else {
